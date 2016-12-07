e6abd14c25e0b0e6c696e6798fef39dbb6382f01
Anastasia Gaydashenko <a.v.gaydashenko@gmail.com>
2016-12-07 22:01:14 +0300

    removed all

--- main.py
+++
@@ -1,9 +0,0 @@
-from server import Server
-from client import Client
-from interface import Interface
-import logging
-
-logging.basicConfig(level=logging.DEBUG, format="%(filename)s, line:%(lineno)d # [%(asctime)s] %(message)s")
-logging.info("start session")
-
-Interface(handle_server=Server, handle_client=Client)
